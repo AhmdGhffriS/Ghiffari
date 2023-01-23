77b3c10923ecd573e9014e65c0f594c2a49c41bb
Ahmad Ghiffari Sya'Ban <122697120+AhmdGhffriS@users.noreply.github.com>
2023-01-23 08:43:57 -0800

    Update Tugas2_Program_Shopping_Online.cpp
    masih Error di pilihan menu 3 ( case 3 )

--- Tugas2_Program_Shopping_Online.cpp
+++ Tugas2_Program_Shopping_Online.cpp
@@ -19,58 +19,110 @@ int main () {
 	int pilihan_menu;
 	char ulang;
 	int varian;
-	
+
+	// Awal Pilihan Menu
 	do {
-		cout << "##### Qembung Drink #####" << endl;
-		cout << "~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
-		cout << endl;
-		cout << "Pilihan Menu :" << endl;
-		cout << "1. Lihat Pricelist" << endl;
-		cout << "2. Cari Harga barang" << endl;
-		cout << "3. Mulai Order" << endl;
-		cout << "4. Checkout" << endl;
-		cout << "5. Cetak Receipt" << endl;
+		cout << "<><><><><><><><><><><><><><><><><>" << endl;
+		cout << "<>        Qembung Drink         <>" << endl;
+		cout << "<><><><><><><><><><><><><><><><><>" << endl;
+		cout << "<>~~~~~~~~~ Menu Utama ~~~~~~~~~<>" << endl;
+		cout << "<>     1. Pricelist Minuman     <>" << endl;
+		cout << "<>     2. Cari Harga barang     <>" << endl;
+		cout << "<>        3. Mulai Order        <>" << endl;
+		cout << "<>          4. Checkout         <>" << endl;
+		cout << "<>       5. Cetak Receipt       <>" << endl;
+		cout << "<><><><><><><><><><><><><><><><><>" << endl;
+
 		cout << "Masukan Pilihan Menu : ";
 		cin >> pilihan_menu;
-		
+		cout << "##################################" << endl;
 		cout << endl;
+		
 		switch (pilihan_menu) {
-			case 1 :
-				cout << "===== Daftar Minuman =====" << endl;
-				cout << "1. ThaiTea		: Rp.  7000" << endl;
-				cout << "2. GreenTea		: Rp.  8000" << endl;
-				cout << "3. Coklat Creamy	: Rp. 13000" << endl;
-				cout << "4. Susu Regal		: Rp. 15000" << endl;
-				cout << "5. Lemon Madu Soda	: Rp. 15000" << endl;
+			case 1 : // Awal Menu 1
+				cout << "==================================" << endl;
+				cout << "||     Daftar Harga Minuman     ||" << endl;
+				cout << "==================================" << endl;
+				cout << "~ ThaiTea ---------> : Rp.  7000 ~" << endl;
+				cout << "~ GreenTea --------> : Rp.  8000 ~" << endl;
+				cout << "~ CoklatCreamy ----> : Rp. 13000 ~" << endl;
+				cout << "~ SusuRegal -------> : Rp. 15000 ~" << endl;
+				cout << "~ LemonMaduSoda ---> : Rp. 15000 ~" << endl;
+				cout << "==================================" << endl;
 				
-				cout << "Ingin Memilih Menu Lain ?? ( Y/T ) : ";
+				cout << "Kembali Ke Menu Utama ?? ( Y/T ) : ";
 				cin >> ulang;
+				cout << "##################################" << endl;
 				cout << endl;
 				break;
-			case 2 :
+			case 2 : // Awal Menu 2
 				string daftar_barang[] = {"ThaiTea", "GreenTea", "CoklatCreamy", "SusuRegal", "LemonMaduSoda"};
 				int daftar_harga[] = {7000, 8000, 13000, 15000, 15000};
 				int jumlah_elemen = sizeof(daftar_barang)/sizeof(daftar_barang[0]);
 	
 				string key;
 	
-				cout << "Masukan Barang Yang Dicari : ";
+				cout << "Masukan Jenis Minuman Yang Anda Cari : ";
 				cin >> key;
 	
 				int indeksbarang = sequentialsearch(daftar_barang, jumlah_elemen, key);
 				if(indeksbarang==-1) {
-					cout << "harga barang tidak ditemukan" << endl;
+					cout << "Jenis Minuman Tidak Ditemukan" << endl;
 				}
 				else {
-					cout << "Harga Barang Yang Di Cari Adalah : " << daftar_harga[indeksbarang] << endl;
+					cout << "Harga Jenis Minuman Yang Di Cari Adalah : " << daftar_harga[indeksbarang] << endl;
 				}
-				cout << "Ingin Memilih Menu Lain ?? ( Y/T ) : ";
+				cout << "Kembali Ke Menu Utama ?? ( Y/T ) : ";
 				cin >> ulang;
+				cout << "##################################" << endl;
 				cout << endl;
+				break;
+			case 3 : // Awal Menu 3
+				int jumlah,total=0;
+				int nomor[]= {1,2,3,4,5};
+				int harga[5]= {10000,11000,13000,18000,15000};
+	
+				cout << "==== Minuman Di Toko Qembung ====" << endl;
+				cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << endl;
+				cout << endl;
+				cout << "---------------------------------" << endl;
+				cout << "| Daftar Minuman | Harga | Kode |" << endl;
+				cout << "---------------------------------" << endl;
+				cout << "|     ThaiTea    | 10000 |  01  |" << endl;
+				cout << "|    GreenTea    | 11000 |  02  |" << endl;
+				cout << "|   CoklatCramy  | 13000 |  03  |" << endl;
+				cout << "|    SusuRegal   | 18000 |  04  |" << endl;
+				cout << "|  LemonMaduSoda | 15000 |  05  |" << endl;
+				cout << "---------------------------------" << endl;
+				cout << endl;
+	
+				cout << "Berapa Jenis Minuman Yang Ingin Kamu Beli ?? : ";
+				cin >> jumlah;
+	
+				if(jumlah<1) {
+					cout << "Jenis Minuman Tidak Boleh Kurang Dari 1" << endl;
+					cout << endl;
+				}
+	
+				int input_user [jumlah];
+				int input_jumlah [jumlah];
+	
+				for (int i=0; i<jumlah; i++) {
+					cout << "Masukan Kode Minuman ke-" << i+1 << " : ";
+					cin >> input_user [i];
+					cout << "Masukan Jumlah Minuman Ke-" << i+1 << " : ";
+					cin >> input_jumlah [i];
+					int subtotal = input_jumlah [i] * harga [input_user[i]-1];
+					total = total + subtotal;
+		
+				}
+				cout << "Total Jenis Minuman Yang Anda Beli Adalah : " << jumlah << " Jenis" << endl;
+				cout << "Total Harga Yang Harus Anda Bayar Adalah : Rp. " << total << ",-" << endl;
+				
 		}
 	} while (ulang!= 'T');
 	
-	cout << "Terima Kasih... :)";
+	cout << "Terima Kasih Telah Berkunjung... :)";
 	
 	cout << endl;
 	return 0;
